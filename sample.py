e4f80bce2caa00fd38507418bf8a8e1194b57f99
convexy <shibuya.keigo@gmail.com>
2020-09-26 12:20:28 +0900

    init2

--- sample.py
+++ sample.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from utils.logger import Logger
+import utils.wdb
 
 logger = Logger(__name__)
 
@@ -10,12 +11,12 @@ logger.debug("debug log")
 logger.info("info log")
 
 if "input" == "doesn't match restriction, so ignored":
-  logger.warning("warn log")
+    logger.warning("warn log")
 
 if "input" != "satifies correct conditions":
-  logger.error("error log")
+    logger.error("error log")
 
 try:
-  raise ValueError("vali")
+    raise ValueError("vali")
 except:
-  logger.error("error log", exc_info=True)
+    logger.error("error log", exc_info=True)
