0364164f9b9ddb2d993072276a2727ae7df648ee
convexy <shibuya.keigo@gmail.com>
2020-10-17 14:11:31 +0900

    Init n.

---
+++ utils/qrcode.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Module      : utils.qrcode
+Function    : QRコード作成・読取
+How to use  : from utils.qrcode import QRCode
+              with QRCode() as qrc:
+                qrc.read(...)
+                qrc.create(...)
+Author      : K.Shibuya
+Version     : 2020/09/21 v1.0.0
+History     : 2020/09/21 v1.0.0 K.Shibuya init
+              ****/**/** v*.*.* *.******* ****
+"""
